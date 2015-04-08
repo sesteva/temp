c769fd477da5980185f7355295f81003ea005f52
sesteva <santiagoesteva@gmail.com>
2015-04-08 16:41:14 -0500

    populator is ready from local xml

--- aircraft-populator.js
+++ aircraft-populator.js
@@ -2,6 +2,9 @@ var Firebase = require('firebase');
 var GeoFire = require('geofire');
 var xml2js = require('xml2js');
 var rest = require('restler');
+var fs = require('fs');
+var _ = require('lodash');
+var RSVP = require('rsvp');
 
 var firebusRef = new Firebase('https://nextaircraft-dev.firebaseio.com/');
 var geoFire = new GeoFire(firebusRef.child("_geofire"));
@@ -9,68 +12,56 @@ var geoFire = new GeoFire(firebusRef.child("_geofire"));
 var lastTime = Date.now() - 3600000;
 var updateInterval = 5000;
 
-var agencyList = ['dfw'];
+var location = 'DFW';
 
-function isNumber(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-}
-
-function coerce(n) {
-    return isNumber(n) ? Number(n) : n;
-}
-
-function traverseAndCoerce(o) {
-    var result = {};
-    for (var key in o) {
-        result[key] = coerce(o[key]);
-    }
-    return result;
-}
-
-function vehicleLocation(agency) {
-    var dev = "http://misc.firebase.com/~vikrum/nextbus.xml?dumb&a=";
-    var prod = "http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=";
-
-    var result = prod + agency + "&t=" + lastTime;
-    lastTime = Date.now();
-
-    return result;
-}
-
-
-function updateFirebaseWithData() {
-    agencyList.forEach(function (agency) {
-        rest.get(vehicleLocation(agency)).on('complete', function (data) {
-            var parser = new xml2js.Parser();
-            console.log(data);
-            parser.parseString(data, function (err, result) {
-                if (result && result.body && result.body.aircraft) {
-                    var i = 0;
-                    result.body.aircraft.forEach(function (item) {
-                        var vehicle = item['$'];
-                        var geoPosition = {};
-                        if (vehicle && vehicle.id) {
-                            var firebaseId = 21;
-                            vehicle = traverseAndCoerce(vehicle);
-                            vehicle.timestamp = (Date.now() / 1000) - vehicle.secsSinceReport;
-                            vehicle.vtype = 'FJLMNXKT'.indexOf(vehicle.routeTag) > -1 ? 'train' : 'bus';
-                            firebusRef.child(agency).child(firebaseId).set(vehicle);
-                            geoPosition[agency + firebaseId] = [32.899809, -97.040335]
-                            geoFire.set(getPosition);
-
-                        }
-                        else {
-                            console.log("bad vehicle ->");
-                            console.log(vehicle);
-                        }
-                        i++;
-                    });
-                }
+function getData(){
+    var promise = new RSVP.Promise(function(resolve, reject) {
+        var parseString = require('xml2js').parseString;
+        fs.readFile('mock.xml', 'utf8', function (err,data) {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            parseString(data, function (err, result) {
+                resolve(result);
             });
         });
     });
+    return promise;
+}
+
+function createAircraft(data){
+    var aircraft = data['$'];
+
+    if (aircraft && aircraft.id) {
+        aircraft.id = parseInt(aircraft.id);
+        aircraft.lat = parseInt(aircraft.lat);
+        aircraft.lon = parseInt(aircraft.lon);
+        aircraft.geoKey = location + ':' + aircraft.id;
+        aircraft.timestamp = (Date.now() / 1000) - aircraft.secsSinceReport;
+        aircraft.inbound = (aircraft.origin && aircraft.origin.indexOf(location) > -1) ? true : false;
+
+        //save model to firebase
+        firebusRef.child(location).child(aircraft.id).set(aircraft);
+        //save geohash to firebase
+        geoFire.set(aircraft.geoKey, [aircraft.lat, aircraft.lon]);
+    }
+    else {
+        console.log("bad aircraft ->");
+        console.log(aircraft);
+    }
+}
+
+function updateFirebase(){
+    getData().then(function(result){
+        if (result && result.body && result.body.aircraft) {
+            _.forEach(result.body.aircraft, createAircraft);
+        }
+    }, function(err){
+        console.log(err);
+    })
 }
 
 setInterval((function () {
-    updateFirebaseWithData();
+    updateFirebase();
 }), updateInterval);
