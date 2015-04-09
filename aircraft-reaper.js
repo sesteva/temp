c4afe5e9eb96fdfbff8460fc60494498dd4c8b6a
sesteva <santiagoesteva@gmail.com>
2015-04-09 09:36:38 -0500

    aircraft reaper by age

--- aircraft-reaper.js
+++ aircraft-reaper.js
@@ -4,29 +4,38 @@ var Firebase = require('firebase');
 var GeoFire = require('geofire');
 
 
-var firebusRef = new Firebase('https://nextaircraft-dev.firebaseio.com/');
+var firebaseRef = new Firebase('https://nextaircraft-dev.firebaseio.com/');
+var geoFire = new GeoFire(firebaseRef.child("_geofire"));
 var updateInterval = 200;
 var reapAge = 400;
-var agencyList = [ 'dfw' ];
+var locations = ['DFW'];
 
 // This method removes old node entries based on attribute ts
 function reap() {
-  var runTs = Date.now() / 1000;
-  agencyList.forEach(function(item) {
-    firebusRef.child(item).once('value', function(s) {
-       s.forEach(function(busSnap) {
-         var age = runTs - busSnap.val().timestamp;
-	 if(age > reapAge) {
-           busSnap.ref().remove();
-           //geofire remove
-	 }
-       });
+    var runTs = Date.now() / 1000;
+    locations.forEach(function (item) {
+        firebaseRef.child(item).once('value', function (s) {
+            s.forEach(function (aircraftSnapshot) {
+                var age = runTs - aircraftSnapshot.val().timestamp;
+                console.log(age);
+                console.log(reapAge);
+                if (age > reapAge) {
+                    console.log(aircraftSnapshot);
+                    var geoKey = aircraftSnapshot.val().geoKey;
+                    aircraftSnapshot.ref().remove();
+                    geoFire.remove(geoKey).then(function() {
+                        console.log("Provided key has been removed from GeoFire");
+                    }, function(error) {
+                        console.log("Error: " + error);
+                    });
+                }
+            });
+        });
     });
-  });
 }
 
 reap();
 
-setInterval((function() {
-  reap();
+setInterval((function () {
+    reap();
 }), updateInterval);
