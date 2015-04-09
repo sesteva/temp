e506bcdefe4a2b4ceb5a0bc11c18eac230de64cf
sesteva <santiagoesteva@gmail.com>
2015-04-09 10:09:31 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,17 +1,41 @@
-# firebase-populator
+# Next Aircraft
 
 Set of scripts to poll the FE API to populate a Firebase endpoint.
 
+## firebase-populator
+
+The populator consumes a WS and populates the model and the geohash to Firebase.
+This is done every 5000 ms.
+
+## firebase-reaper
+
+The reaper queries Firebase and evaluates if model/geohash is stale based on timestamp.
+This is done every 200 ms.
 
 ## TODO
+
+For testing purposes, both the populator and repeat have been using a 'dev' firebase repo.
+https://nextaircraft-dev.firebaseio.com/
+
+This is accessible from same user account:
+
+user: santiago.esteva@sabre.com
+pass: esteva81
+
+Once the WS is connected then update both populator and repear to use https://geoaircraft.firebaseio.com/
 
 Populator:
 
 - Retrieve from WS instead of local file
+- Pay the price of fast prototyping....write the tests!!!!!
 
 Reaper:
 
-- Everything todo
+- Pay the price of fast prototyping....write the tests!!!!!
+
+Script:
+
+- Create a shell script to run both on separate threads.
 
 ## Run Populator locally
 
--- aircraft-reaper.js
+++ aircraft-reaper.js
@@ -3,24 +3,20 @@
 var Firebase = require('firebase');
 var GeoFire = require('geofire');
 
-
 var firebaseRef = new Firebase('https://nextaircraft-dev.firebaseio.com/');
 var geoFire = new GeoFire(firebaseRef.child("_geofire"));
 var updateInterval = 200;
-var reapAge = 400;
+var reapAge = 400; // 6 mins. We want to avoid flashing elements
 var locations = ['DFW'];
 
-// This method removes old node entries based on attribute ts
+// This method removes old node entries based on attribute timestamp
 function reap() {
     var runTs = Date.now() / 1000;
     locations.forEach(function (item) {
         firebaseRef.child(item).once('value', function (s) {
             s.forEach(function (aircraftSnapshot) {
                 var age = runTs - aircraftSnapshot.val().timestamp;
-                console.log(age);
-                console.log(reapAge);
                 if (age > reapAge) {
-                    console.log(aircraftSnapshot);
                     var geoKey = aircraftSnapshot.val().geoKey;
                     aircraftSnapshot.ref().remove();
                     geoFire.remove(geoKey).then(function() {
